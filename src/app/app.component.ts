65a4a2aa7dfc0f304288f244b4307805627b8467
cvcastano <carocastanocastillo@gmail.com>
2021-08-31 11:17:38 +0200

    Add open/close nav functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() {
+    this.opened = true;
+}
+
   title = "Angular Sandbox";
+  events: string[] = [];
+  opened: boolean;
+
+
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
